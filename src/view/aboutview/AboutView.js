3117cfb8417a45954c66b34e9cce3536f254b773
Mathilda Serneblad <70764603+mathildaserne@users.noreply.github.com>
2021-10-20 13:28:12 +0200

    ändrat

--- src/view/aboutview/AboutView.js
+++ src/view/aboutview/AboutView.js
@@ -7,20 +7,25 @@ import foodLogotype2 from '../../shared/images/foodLogotype2.jpg'
 export const AboutView = () => {
     return (
     <div>
-      <h2>Denna sida är för hemma kockar som behöver lite inspiration!</h2>
-      <h4>Enkla, snabba och festliga recept. Allt från festmat till pannkakor, kladdkaka och vegetarisk mat.</h4>
+      <h3>Denna sida är för hemma kockar som behöver lite inspiration, 
+        med mat för alla tillfällen.</h3>
+      <h4>Vi har samlat middagstips för så väl jäktiga vardagar och 
+        de dagar då du vill njuta lite extra av middagen. 
+        En bra vardagsmiddag går att laga snabbt och enkelt, 
+        samtidigt som resultatet bjuder på läckra smaker. 
+        Var så god, våra middagsrecept!</h4>
       <div class="row">
         <div class="column">
-          <img src={foodLogotype} alt={'..error'}/>
+          <img class="img" src={foodLogotype} alt={'..error'}/>
         </div>
         <div class="column">
-          <img src={foodLogotype1} alt={'..error'}/>
+          <img class="img" src={foodLogotype1} alt={'..error'}/>
         </div>
         <div class="column">
-          <img src={foodLogotype2} alt={'..error'}/>
+          <img class="img" src={foodLogotype2} alt={'..error'}/>
         </div>
-        <h4>Gå till Search där du enkelt anger de ingredienser du har hemma 
-        med resultat av flertal olika lätt lagade och goda recept.</h4>
+        <h4>Gå till "Search" där du enkelt kan söka på 
+          ingrediens eller recept.</h4>
       </div>
     </div>
     )
