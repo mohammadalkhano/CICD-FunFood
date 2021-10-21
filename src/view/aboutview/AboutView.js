4b5a2d9fa1aa2053306b080b3e2c6b8a1fd30b81
Mathilda Serneblad <70764603+mathildaserne@users.noreply.github.com>
2021-10-21 12:27:46 +0200

    jobbat på css

--- src/view/aboutview/AboutView.js
+++ src/view/aboutview/AboutView.js
@@ -10,53 +10,55 @@ export const AboutView = () => {
       <h3>FunFood är för hemma kockar som behöver lite inspiration, <br/>
         med mat för alla tillfällen.</h3>
 
-          <img  class="img1" src={foodLogotype} alt={'..error'}/>
-
       <div class="div1">
-      <h4 class="h41">Vi har samlat middagstips för så väl jäktiga vardagar <br/>
-                      och de dagar då du vill njuta lite extra av middagen. <br/>
-                      En bra vardagsmiddag går att laga snabbt och enkelt, <br/>
-                      samtidigt som resultatet bjuder på läckra smaker. <br/>
+        <img class = "img1" src={foodLogotype} alt={'..error'}/>                    
+        <h4 class="h41">Välj bland våra receptfavoriter och dela vår 
+                      passion för den godaste maten. Här finns middagstips, 
+                      goda bakverk, mättande mellis och festmenyer. 
+                      Vi har samlat middagstips för så väl jäktiga vardagar 
+                      och de dagar då du vill njuta lite extra av middagen. 
+                      En bra vardagsmiddag går att laga snabbt och enkelt, 
+                      samtidigt som resultatet bjuder på läckra smaker. 
+                      Allt från enkla vardagsrätter till en lyxigare middag 
+                      med passande vin. Alla våra recept är provlagade av våra kockar.
                       Var så god, våra middagsrecept!</h4>
       </div>
 
-          <img  class="img2" src={foodLogotype1} alt={'..error'}/>
-
       <div class="div2">
-        <h4 class="h42">God mat, helt enkelt. Det ska vara enkelt att laga  <br/>
-                        god mat, det har vi alltid tyckt. <br/>
-                        Oavsett om du letar efter tips på en snabb lunchrätt  <br/>
-                        eller en festlig finmiddag. Vare sig du är sugen på  <br/>
-                        en husmansklassiker eller den senaste trendmaten. Både som  <br/>
-                        inspiration till sommarens grillkvällar och en  <br/>
-                        vintermiddag med levande ljus. Hos FunFood hittar du  <br/>
-                        massor av spännande rätter som passar alla tillfällen. <br/>
-                        Alla provlagade, näringsberäknade och med tydliga beskrivningar <br/>
-                        som gör det enkelt att lyckas i köket. Och vi fyller  <br/>
-                        ständigt på med fler. <br/>
+        <img class = "img2" src={foodLogotype1} alt={'..error'}/>
+        <h4 class="h42">God mat, helt enkelt. Det ska vara enkelt att laga  
+                        god mat, det har vi alltid tyckt.
+                        Oavsett om du letar efter tips på en snabb lunchrätt  
+                        eller en festlig finmiddag. Vare sig du är sugen på  
+                        en husmansklassiker eller den senaste trendmaten. Både som  
+                        inspiration till sommarens grillkvällar och en  
+                        vintermiddag med levande ljus. Hos FunFood hittar du  
+                        massor av spännande rätter som passar alla tillfällen. 
+                        Alla provlagade, näringsberäknade och med tydliga beskrivningar 
+                        som gör det enkelt att lyckas i köket.
+                        Och vi fyller ständigt på med fler. <br/>
                         Bra recept på god mat, helt enkelt.</h4>
       </div>
 
-          <img class="img3" src={foodLogotype2} alt={'..error'}/>
-
       <div class="div3">
-      <h4 class="h43">FunFood samlar tusentals favoritrecept från  <br/>
-                      Aller Medias tidningar tillsammans med helt  <br/>
-                      nyutvecklade recept. Har du kycklingfiléer <br/>
-                      i frysen eller mangold i trädgårdslandet?  <br/>
-                      Hos FunFood kan du enkelt söka efter recept med  <br/>
-                      dina favoritingredienser. Eller fritt söka inspiration bland <br/>
-                      våra tydliga kategorier. Alla våra recept är  <br/>
-                      näringsberäknade och visar balansen mellan protein,  <br/>
-                      fett och kolhydrater. Och för dig som allergisk <br/>
-                      eller överkänslig finns enkel och tydlig information. <br/>
-                      FunFood fylls kontinuerligt på med många nya spännande  <br/>
-                      rätter att prova.</h4> 
+        <img class = "img3" src={foodLogotype2} alt={'..error'}/>
+        <h4 class="h43">FunFood samlar tusentals favoritrecept från  
+                      Aller Medias tidningar tillsammans med helt  
+                      nyutvecklade recept. Har du kycklingfiléer 
+                      i frysen eller mangold i trädgårdslandet?  
+                      Hos FunFood kan du enkelt söka efter recept med  
+                      dina favoritingredienser. Eller fritt söka inspiration bland
+                      våra tydliga kategorier. Alla våra recept är  
+                      näringsberäknade och visar balansen mellan protein,  
+                      fett och kolhydrater. Och för dig som allergisk 
+                      eller överkänslig finns enkel och tydlig information.
+                      FunFood fylls kontinuerligt på med många nya
+                      spännande rätter att prova.</h4> 
       </div>
 
-      <h4 class="h44">Gå till "Recipies" där du enkelt kan söka på 
-          ingrediens eller recept.</h4>
-          
+      <div>
+        <h4 class="h44">Gå till "Recipies" där du enkelt kan söka på ingrediens eller recept.</h4>
+      </div>
           <a class="jump-to-top" href="#">Jump To Top</a>
     </div>
 
