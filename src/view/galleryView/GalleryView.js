106a71db330d6704a4231d0c378bb172a9f91de2
preetidogra <70689452+preetidogra@users.noreply.github.com>
2021-10-30 10:56:21 +0200

    gallery View changes

--- src/view/galleryView/GalleryView.js
+++ src/view/galleryView/GalleryView.js
@@ -6,8 +6,31 @@ export const GalleryView = () => {
 
 	return (
 		<div className="container">
-            <h1>GalleryView</h1>
+            <h3>Gallery View</h3>
+			
+			<div>
+			</div>
+			
+			<div classname= "image">
+			<div className="picture Pic1"></div>
+			<div className="picture Pic2"> </div>
+			<div className="picture Pic3"> </div>
+			<div className="picture Pic5"> </div>
+			<div className="picture Pic6"> </div>			
+			<div className="picture Pic7"> </div>			
+			<div className="picture Pic8"> </div>
+			
+			</div>
+
 		</div>
+
+		
+
+		
+
+		
+
+		
 
 	)
 }
