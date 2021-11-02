42e2a10f920a4993f3a014b932fced11a5f3bf9f
preetidogra <70689452+preetidogra@users.noreply.github.com>
2021-11-02 09:34:38 +0100

    Unit testing in contactView page

---
+++ src/view/homeview/__test__/HomeView.test.js
@@ -0,0 +1,20 @@
+//import Enzyme, { shallow } from 'enzyme'
+//import Adapter from 'enzyme-adapter-react-16'
+//import HomeView from '../HomeView'
+
+//Enzyme.configure({adapter: new Adapter()});
+
+/*describe('HomeView', () => {
+    it('should be true', () =>{
+        const prov =true;
+        expect(prov).toBe(true);
+    
+    });
+
+    it('should be false', () =>{
+        const prov =true;
+        expect(prov).toBe(false);
+    })
+
+});
+*/
