f8d4009e10f8b822777704a987e790df6c490b7a
ziaalekozai <88ziaale@gafe.molndal.se>
2021-11-04 11:17:21 +0100

    Jobbade på HomeView

--- src/view/homeview/HomeView.js
+++ src/view/homeview/HomeView.js
@@ -16,19 +16,17 @@ export const HomeView = () => {
 					<h1>Welcome to FunFood !</h1>
 
 					<p className="home-p">Dear User, kindly visit our Recipes Page to have a look at some of the best recipes from across the world. Please contact us with your valuable feedback to help us improve.</p>
-						<button className="home-button gallery-button " onClick={() => hestory.push(RoutingPath.recipeView)}>Search recipes</button>
-						<button className="home-button search-button" onClick={() => hestory.push(RoutingPath.galleryView)}>Gallery</button>
 				</div>
 				<div className="col">
-					<div className="card card1" onClick={() => hestory.push(RoutingPath.galleryView)}>
+					<div className="card card1" >
 					</div>
-					<div className="card card2" onClick={() => hestory.push(RoutingPath.galleryView)}>
+					<div className="card card2" >
 					</div>
-					<div className="card card3" onClick={() => hestory.push(RoutingPath.galleryView)}>
+					<div className="card card3" >
 					</div>
-					<div className="card card4" onClick={() => hestory.push(RoutingPath.galleryView)}>
+					<div className="card card4" >
 					</div>
-					<div className="card card5" onClick={() => hestory.push(RoutingPath.galleryView)}>
+					<div className="card card5" >
 					</div>
 				</div>
 			</div>
