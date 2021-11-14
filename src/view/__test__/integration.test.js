ccd69ac19a57452b7a3f6958bfac0fe49ba26755
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-11-14 22:09:37 +0100

    deleted code comments

--- src/view/__test__/integration.test.js
+++ src/view/__test__/integration.test.js
@@ -31,34 +31,11 @@ describe('views integration', () => {
     expect(item).toBeInTheDocument();
     userEvent.click(item);
     expect(history.location.pathname).toBe('/recipe');
-    
-    
-
     const item1 = screen.getByText('About');
     expect(item1).toBeInTheDocument();
     userEvent.click(item1);
     expect(history.location.pathname).toBe('/about');
     //console.log(item1);
-
-
-
-    // const mockChange = jest.fn();
-    // let wrapper = mount(<RecipeView></RecipeView>);
-    // const result = wrapper.find('.standard-bare');
-    // console.log(result.debug);
-    // result.simulate('change').find('.standard-bare');
-    // expect(mockChange.mock.calls).lastCalledWith('chiken');
-
-
-
-
-    // console.log(wrapper.debug);
-
-    //expect(history.length).toBe(2);
-    //expect(history.location.pathname).toBe('/about');
-    //   wrapper.find().simulate();
-    //   console.log(wrapper.debug);
-    //expect(screen.getByText(/you are home/i)).toBeInTheDocument()
-    //expect(wrapper.getElements.name('some fake name')).toBeInTheDocument();
+    
   });
 });
